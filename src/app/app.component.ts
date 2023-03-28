ab5e9d5f26d24617a789403eaf7e09f61b099904
Sandro T <ordysan@gmail.com>
2023-03-28 20:51:38 +0200

    add stripe functions to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
 
 @Component({
 	selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'ng-stripe';
+  testproduct = {'name':'s', "x": "z"};
+  private response: any;
+
+  constructor(private http: HttpClient){}
+
+  async triggerCreateCheckout(eventProduct: any) {
+    this.response = await this.http
+      .post('/.netlify/functions/stripe.js', eventProduct, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).toPromise();
+    this.openStripe(this.response);
+  }
+
+    openStripe = async (stripeParams: any) => {
+    const stripe = await loadStripe(stripeParams.publishableKey);
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: stripeParams.sessionId,
+    });
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +14,7 @@ import {MatCardModule} from '@angular/material/card';
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
