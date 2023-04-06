770b91e1ca41e9304cb0385ee1d435b06fc2913f
Sano <ordysan@gmail.com>
2023-04-06 20:50:46 +0200

    edit Access Control header settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,39 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { loadStripe } from '@stripe/stripe-js';
-import items from '../assets/items.json'
+import items from '../assets/items.json';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.sass']
+	styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-	title = 'ng-stripe';
-  testproduct = items[1];
-  private response: any;
+	title = 'Stripe payment';
+	testproduct = items[1];
+	private response: any;
+	private headers = new HttpHeaders()
+		.set('Content-Type', 'application/json')
+		.set('Access-Control-Allow-Origin', '*')
+		.set('Access-Control-Allow-Credentials', 'true')
+		.set('Access-Control-Allow-Methods', 'POST')
+		.set('Access-Control-Allow-Headers', 'Content-Type')
+		.set('Authorization', '*');
 
-  constructor(private http: HttpClient){}
+	constructor(private http: HttpClient) {}
 
-  async triggerCreateCheckout(eventProduct: any) {
-    console.log(`object -> ${this.testproduct}`);
-    this.response = await this.http
-      .post('/.netlify/functions/stripe', eventProduct, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).toPromise();
-    this.openStripe(this.response);
-  }
+	async triggerCreateCheckout(eventProduct: any) {
+		console.log(`object -> ${this.testproduct}`);
+		this.response = await this.http
+			.post('/.netlify/functions/stripe', eventProduct, { headers: this.headers })
+			.toPromise();
+		this.openStripe(this.response);
+	}
 
-    openStripe = async (stripeParams: any) => {
-    const stripe = await loadStripe(stripeParams.publishableKey);
-    const { error } = await stripe!.redirectToCheckout({
-      sessionId: stripeParams.sessionId,
-    });
-  };
+	openStripe = async (stripeParams: any) => {
+		const stripe = await loadStripe(stripeParams.publishableKey);
+		const { error } = await stripe!.redirectToCheckout({
+			sessionId: stripeParams.sessionId,
+		});
+	};
 }
